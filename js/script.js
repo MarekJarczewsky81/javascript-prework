6a52172de26a57df40e675d598f34a1e386a520c
Marek Jarczewski <marek.jarczewski81@gmail.com>
2024-02-28 04:08:57 +0100

    Version 3 - stable

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const buttonRock = document.getElementById("button-rock");
 const buttonPaper = document.getElementById("button-paper");
 const buttonScissors = document.getElementById("button-scissors");
-// const counterPlayer = document.getElementById("counter-player");
-// const counterComputer = document.getElementById("counter-computer");
+const counterPlayer = document.getElementById("counter-player");
+const counterComputer = document.getElementById("counter-computer");
+
+let counterPl = 0;
+let counterCom = 0;
 
 function buttonClicked(argButtonName) {
   clearMessages();
@@ -13,40 +16,51 @@ function buttonClicked(argButtonName) {
     const computerMove = getMoveName(randomNumber);
   printMessage("Ruch komputera to: " + computerMove);
     displayResult(playerMove, computerMove);
-  
+    printResults();
+  printMessage("Twoje punkty:" (counterPl)) 
+  printMessage("Punkty komputera:"  (counterCom));
 
   
   function getMoveName(randomNumber) {
-    if (randomNumber == 1) {
+    if (randomNumber === 1) {
       return "kamień";
-    } else if (randomNumber == 2) {
+    } else if (randomNumber === 2) {
       return "papier";
-    } else if (randomNumber == 3) {
+    } else if (randomNumber === 3) {
       return "nożyce";
     } else {
     printMessage(
       "Nie znam ruchu o id " + randomNumber + ""
       );
-      return "nieznany ruch";
+      return printMessage("nieznany ruch");
     }
   }
 
-  
-
   function displayResult(playerMove, computerMove) {
-  if (playerMove === "papier" && computerMove === "kamień") {
-    printMessage("Wygrywasz!");
-  } else if (playerMove === "nożyce" && computerMove === "papier") {
-    printMessage("Wygrywasz!");
-  } else if (playerMove === "kamień" && computerMove === "nożyce") {
-    printMessage("Wygrywasz!");
-  } else if (playerMove === computerMove) {
-    printMessage("Remis!");
-  } else {
-    printMessage("Przegrywasz!");
-  }    
+    if (playerMove === "papier" && computerMove === "kamień") {
+      printMessage("Wygrywasz!");
+      counterPl = counterPl + 1;
+    } else if (playerMove === "nożyce" && computerMove === "papier") {
+      printMessage("Wygrywasz!");
+      counterPl = counterPl + 1;
+    } else if (playerMove === "kamień" && computerMove === "nożyce") {
+      printMessage("Wygrywasz!");
+      counterPl = counterPl + 1;
+    } else if (playerMove === computerMove) {
+      printMessage("Remis!");
+    } else {
+      printMessage("Przegrywasz!");
+      counterCom = counterCom + 1;
+    }    
+  }
+  
+  function printResults () {
+    counterPlayer.innerHTML = counterPl;
+    counterComputer.innerHTML = counterCom;
+    
+  }
 }
-}
+
 buttonRock.addEventListener("click", function () {
   buttonClicked("kamień");
 });
